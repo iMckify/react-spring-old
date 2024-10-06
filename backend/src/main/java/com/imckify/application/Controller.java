d79324a009387db69cb94165b462953c2c2b2c0b
iMckify <rokas.mockevicius@gmail.com>
2024-10-06 20:51:08 +0300

    backend

---
+++ backend/src/main/java/com/imckify/application/Controller.java
@@ -0,0 +1,18 @@
+package com.imckify.application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class Controller {
+
+    private static final String template = "Hello there, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/hello")
+    public String greeting() {
+        return String.format(template, counter.incrementAndGet());
+    }
+}
