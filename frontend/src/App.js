1cfd15f055fbe59dce76c11fd109953a1d4a0203
iMckify <rokas.mockevicius@gmail.com>
2024-10-31 19:24:14 +0200

    wolfi minor fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! This is wolfi.
+          Hello Rokas! wolfi migration complete.
         </p>
         <a
           className="App-link"
