dc9f67633f82387f492d7450393074dbfc67414f
iMckify <rokas.mockevicius@gmail.com>
2024-11-11 22:56:40 +0200

    GH_ACCESS_TOKEN 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! checking secret.
+          Hello Rokas! checking secret 2.
         </p>
         <a
           className="App-link"
