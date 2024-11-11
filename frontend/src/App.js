f80bbe0af27e9ad93c844f52e107763f4be557df
iMckify <rokas.mockevicius@gmail.com>
2024-11-11 23:28:44 +0200

    8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! checking secret 7.
+          Hello Rokas! checking secret 8.
         </p>
         <a
           className="App-link"
