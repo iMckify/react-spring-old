a03a8b959d851d5b4090b70fa0b01b556982e615
iMckify <rokas.mockevicius@gmail.com>
2024-11-11 23:53:10 +0200

    in pr job

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! checking secret 8.
+          Hello Rokas! checking secret in pr job.
         </p>
         <a
           className="App-link"
