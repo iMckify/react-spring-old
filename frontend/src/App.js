ae8c2ae21d4c1b6c41fa62a4fe46558389f47016
iMckify <rokas.mockevicius@gmail.com>
2024-12-30 12:26:11 +0200

    redeploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! gh pages
+          Hello Rokas! redeploy
         </p>
         <a
           className="App-link"
