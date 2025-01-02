20523b6a8de576924743eca26b8dba9bd9020ae9
iMckify <rokas.mockevicius@gmail.com>
2025-01-02 13:07:56 +0200

    junit failing test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! `/test pr` triggered only on demand by github action
+          Hello Rokas! tests
         </p>
         <a
           className="App-link"
