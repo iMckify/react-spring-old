e7d4a91a48f03f8c2150906b2e9a882f0b129fe6
iMckify <rokas.mockevicius@gmail.com>
2025-01-02 10:40:53 +0200

    fix commenting 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! made some changes
+          Hello Rokas! trigger only after comment
         </p>
         <a
           className="App-link"
