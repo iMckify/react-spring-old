27670a85d1a762eb543519a5db6493d43c85f75e
iMckify <rokas.mockevicius@gmail.com>
2025-01-02 09:56:40 +0200

    trigger3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Rokas! redeploy
+          Hello Rokas! custom trigger
         </p>
         <a
           className="App-link"
